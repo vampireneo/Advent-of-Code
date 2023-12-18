f0acaee08c059e7c711d21718523d33f45ecee27
Neo Choi <neo.choi@gropyus.com>
2023-12-18 17:56:14 +0100

    feat: 2023 day 17 (wip)

---
+++ 2023/17/index.js
@@ -0,0 +1,13 @@
+import { readFileSync } from 'fs'
+const rawData = readFileSync('test.txt', { encoding: 'utf-8' }).trim()
+// const rawData = readFileSync('input.txt', { encoding: 'utf-8' }).trim()
+
+const data = rawData.split('\n').map((r) => r.split('').map(parseFloat))
+
+const part1Answer = data
+
+console.log(part1Answer)
+
+// const part2Answer = data
+
+// console.log(part2Answer)
